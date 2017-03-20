e12b64ea6c2644d8d5f20e41c5045c6c5d6aee38
Christian Storch <ccstorch@gmail.com>
2017-03-20 16:19:55 +0000

    Init

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+import '../public/css/normalize.css';
+import '../public/css/wf.css';
+import '../public/css/app.wf.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
